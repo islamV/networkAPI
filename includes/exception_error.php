127767fe6ab41d64f95d8c9f205c06edf4596314
islamV <islamabdelkarimv@gmail.com>
2024-07-26 18:58:15 +0300

    ups

--- includes/exception_error.php
+++ includes/exception_error.php
@@ -10,15 +10,35 @@ header("Content-Type: application/json");
 $GET_ROUTES = isset($routes['GET']) ? $routes['GET'] : [];
 $POST_ROUTES = isset($routes['POST']) ? $routes['POST'] : [];
 
-
-if( $_SERVER["REQUEST_METHOD"] == "GET" &&!in_array(segment(), array_column($GET_ROUTES, 'segment'))  ) {
-    echo json_encode(["status" => "error", "message" => "Route not found"]);
-    http_response_code(404);
-    exit();
+function isUserAuthenticated() {
+    return null !== session('user_data');
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST" && !in_array(segment(), array_column($POST_ROUTES , 'segment')) ) {
-    echo json_encode(["status" => "error", "message" => "Route not found"]);
-    http_response_code(404);
-    exit();
+if( $_SERVER["REQUEST_METHOD"] == "GET"  ) {
+
+    if (!in_array(segment(), array_column($GET_ROUTES, 'segment'))  && isUserAuthenticated()) {
+        echo json_encode(["status" => "error", "message" => "Route not found"]);
+        http_response_code(404);
+        exit();
+    }elseif( !isUserAuthenticated()){
+        echo json_encode(["status" => "error", "message" => "Unauthorized"]);
+        http_response_code(404);
+        exit();
+    }
+  
+}
+
+if( $_SERVER["REQUEST_METHOD"] == "POST"  ) {
+
+    if (!in_array(segment(), array_column($POST_ROUTES, 'segment'))  && isUserAuthenticated()) {
+        echo json_encode(["status" => "error", "message" => "Route not found"]);
+        http_response_code(404);
+        exit();
+    }
+    elseif( !in_array(segment(), array_column($POST_ROUTES, 'segment'))  && !isUserAuthenticated()){
+        echo json_encode(["status" => "error", "message" => "Unauthorizeds"]);
+        http_response_code(404);
+        exit();
+    }
+  
 }
--- v1/getOffers.php
+++ v1/getOffers.php
@@ -4,16 +4,22 @@ header("Content-Type: application/json");
 $user  = session('user_data');
 $admin = $user['created_by'];
 
-
 $offers = getDB(  'networks_profiles',
 "WHERE network_admin = '$admin' 
 AND enable_status !=1 AND total_qouta != 0 "
 );
+// var_dump($offers);
 
 
 
-
+if($offers ){
 echo json_encode(array('status' => 'success',
     'offers' => $offers ,
     'count' => $offers['count']
  ));
+  http_response_code(200);
+}else{
+echo json_encode(["status" => "error", "message" => "data not found."]);
+        http_response_code(404);
+        exit;
+}
